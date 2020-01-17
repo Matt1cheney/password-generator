bda731be94692f787427df547b439917f0c248ba
Matthew Cheney <matt1cheney@gmail.com>
2020-01-17 17:14:07 -0500

    made password string come back random

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");   
+var passwordText = document.querySelector("#password");
 
 // main structure
 let password = document.getElementById('password');
@@ -12,15 +12,74 @@ let symbols = document.getElementById('symbols');
 let generate = document.getElementById('generate');
 let clipboard = document.getElementById('clipboard');
 
+// functions on bottom
 let randomFunction = {
   upper: getRandomUpper,
   lower: getRandomLower,
   number: getRandomNumber,
   symbol: getRandomSymbol,
+} 
+// object constructor methods
+// Object.keys(randomFunction).forEach(function(key){
+// console.log(key);
+// console.log(randomFunction[key]);
+// });
 
 
+// Write password to the #password input
+function writePassword(upper, lower, number, symbol, length) {
+  
+  
+  let writePassword = '';
+  
+  let typesCount = upper + lower + number + symbol;
+  
+  if (length < 8 || length > 128) {
+    return alert('not enough characters or too many characters');
+  }
+  if (typesCount === 0) {
+    return alert('you need to select a type');
+  }
+
+  let typesArr = [{ upper }, { lower }, { number }, { symbol }].
+  //filter out objects that are unchecked
+  filter(
+    item => Object.values(item)[0]);
+    
+    
+    for (let i = 0; i < length; i++) {
+      
+      let funcName = Object.keys(typesArr[Math.floor(Math.random() * typesArr.length)])[0]
+      
+      console.log('funcName', funcName);
+      
+      writePassword += randomFunction[funcName]();
+    };
+    
+    
+    
+let finalPassword = writePassword.slice(0, length);
+return finalPassword;
 }
-// event
+
+
+
+// character functions
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+  let symbols = '!@#$%^&*()_+{}[]<>,.'
+  return symbols[Math.floor(Math.random() * 20)]
+  
+}
+// attached event listener 
 generateBtn.addEventListener('click', () => {
   let length = +numberLength.value;
   let hasUpper = upperCase.checked;
@@ -36,87 +95,20 @@ generateBtn.addEventListener('click', () => {
     length
   );
 });
-
-// Write password to the #password input
-function writePassword(upper, lower, number, symbol, length) {
-  
-
-  let writePassword = '';
-
-  let typesCount = upper + lower + number + symbol;
-
-  //console.log('typesCount', typesCount);
-
-  let typesArr = [{ upper }, { lower }, { number }, { symbol }].
-    //filter out objects that are unchecked
-    filter(
-      item => Object.values(item)[0]);
-
-  //console.log('typesArr', typesArr);
-
-  if (length < 8 || length > 128 ) {
-    return alert('not enough characters or too many characters');
-  }
-  if(typesCount === 0 ) {
-    return alert('you need to select a type');
-  }
-
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
-      let funcName = Object.keys(type)[0]
-      //console.log('funcName', funcName);
-
-      writePassword += randomFunction[funcName]();
-    });
-    
-    
-  }  
-  let finalPassword = writePassword.slice(0, length);
-  return finalPassword;
-}
-
-  
-
-// character functions
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSymbol() {
-  let symbols = '!@#$%^&*()_+{}[]<>,.'
-  return symbols[Math.floor(Math.random() * 20)]
-}
-
-
-
-
 // BONUS 
 
-  copyMe.addEventListener('click', () => {
-    let clipboardText = document.getElementById('password');
-    
+copyMe.addEventListener('click', () => {
+  let clipboardText = document.getElementById('password');
 
-    clipboardText.select();
-    clipboardText.setSelectionRange(0,99999);
-    
-    
-    document.execCommand('copy');
-    
-    alert('Password copied to clipboard');
+  clipboardText.select();
+  clipboardText.setSelectionRange(0, 99999);
+
+  document.execCommand('copy');
+
+  alert('Copied Password \"' + password.textContent + '\" to clipboard'); 
 });
-    
-    
-    
-    
-    
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// BONUS EVENT LISTENER
+
